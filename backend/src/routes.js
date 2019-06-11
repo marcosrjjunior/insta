e24aa7c1609340a81398a08639dc54433fa8a4a9
marcosrjjunior <marcosrjjunior@gmail.com>
2019-06-12 00:26:16 +1000

    adding backend

---
+++ backend/src/routes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const uploadConfig = require('./config/upload')
+const multer = require('multer')
+const routes = new express.Router()
+
+const upload = multer(uploadConfig)
+const PostController = require('./controllers/PostController')
+const LikeController = require('./controllers/LikeController')
+
+routes.get('/posts', PostController.index)
+routes.post('/posts', upload.single('image'), PostController.store)
+
+routes.post('/posts/:id/like', LikeController.store)
+
+module.exports = routes
+// routes.get('/')
