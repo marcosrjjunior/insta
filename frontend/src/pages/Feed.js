4963643e930de048859603992069661a9479941e
marcosrjjunior <marcosrjjunior@gmail.com>
2019-06-15 23:33:46 +1000

    finishing frontend

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import api from '../services/api'
+import io from 'socket.io-client'
 
 import './Feed.css'
 import more from '../assets/more.svg'
@@ -13,9 +14,30 @@ export default class Feed extends Component {
   }
 
   async componentDidMount() {
+    this.registerToSocket()
     const response = await api.get('posts')
 
     this.setState({ feed: response.data })
+  }
+
+  handleLike = id => {
+    api.post(`posts/${id}/like`)
+  }
+
+  registerToSocket = id => {
+    const socket = io('http://localhost:3333')
+
+    socket.on('post', newPost => {
+      this.setState({ feed: [newPost, ...this.state.feed] })
+    })
+
+    socket.on('like', likedPost => {
+      this.setState({
+        feed: this.state.feed.map(post =>
+          post._id === likedPost._id ? likedPost : post,
+        ),
+      })
+    })
   }
 
   render() {
@@ -34,12 +56,14 @@ export default class Feed extends Component {
 
             <img
               src={`http://localhost:3333/files/${post.image}`}
-              alt="picture"
+              alt="post pic"
             />
 
             <footer>
               <div className="actions">
-                <img src={like} alt="" />
+                <button type="button" onClick={() => this.handleLike(post._id)}>
+                  <img src={like} alt="" />
+                </button>
                 <img src={comment} alt="" />
                 <img src={send} alt="" />
               </div>
